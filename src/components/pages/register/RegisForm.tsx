56208835c3011986ce4dd0c8ea2fca605bc328a0
Petrando <petrandorichard@gmail.com>
2024-09-04 09:42:18 +0800

    register form using next UI

--- src/components/pages/register/RegisForm.tsx
+++ src/components/pages/register/RegisForm.tsx
@@ -2,7 +2,7 @@
 import { useState, FormEvent } from "react"
 import { useRouter } from "next/navigation";
 import { Link } from "@nextui-org/react";
-import {Button} from '@nextui-org/button';
+import { Card, Input, Button, CardHeader, CardBody, CardFooter, Chip  } from "@nextui-org/react";
 
 export const RegisForm = () => {
     const [name, setName] = useState("");
@@ -49,41 +49,51 @@ export const RegisForm = () => {
 
     return (
         <div className="grid place-items-center h-screen">
-            <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
-                <h1 className="text-xl font-bold my-4">Register</h1>
-
-                <form onSubmit={handleSubmit} className="flex flex-col gap-3">
-                <input
-                    onChange={(e) => {setName(e.target.value)}}
-                    type="text"
-                    placeholder="Name"
-                />
-                <input
-                    onChange={(e) => {setEmail(e.target.value)}}
-                    type="text"
-                    placeholder="Email"
-                />
-                <input
-                    onChange={(e) => {setPassword(e.target.value)}}
-                    type="password"
-                    placeholder="Password"
-                />
-                <Button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2"
-                    type="submit"
-                >
-                    Register
-                </Button>
-                {error && (
-                    <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-                    {error}
+            <Card className="max-w-[340px]">
+                <CardHeader>
+                    <h1 className="text-xl font-bold my-4">Register</h1>
+                </CardHeader>
+                <CardBody>
+                    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+                        <Input
+                            value={name}
+                            onValueChange={setName}
+                            type="text"
+                            placeholder="Name"
+                        />
+                        <Input
+                            value={email}
+                            onValueChange={setEmail}
+                            type="text"
+                            placeholder="Email"
+                        />
+                        <Input
+                            value={password}
+                            onValueChange={setPassword}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </form>
+                </CardBody>
+                <CardFooter className="flex flex-col items-stretch">
+                    <div className="flex items-center justify-end basis-full">
+                        <Button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2"
+                            type="submit"
+                        >
+                            Register
+                        </Button>
                     </div>
-                )}
+                    {error && (
+                        <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+                        {error}
+                        </div>
+                    )}
 
-                <Link className="text-sm mt-3 text-right" href={"/"}>
-                    Already have an account? <span className="underline">Login</span>
-                </Link>
-                </form>
-            </div>
+                    <Link className="text-sm mt-3 text-right" href={"/"}>
+                        Already have an account? <span className="underline">Login</span>
+                    </Link>
+                </CardFooter>
+            </Card>
         </div>
     )
 }
